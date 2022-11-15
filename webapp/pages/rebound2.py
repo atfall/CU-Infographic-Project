2a2a32113604b70df1e779ad34bcd80cf5b45fc3
atfall <61741874+atfall@users.noreply.github.com>
2022-11-15 16:23:50 +0000

    Update rebound2.py

--- webapp/pages/rebound2.py
+++ webapp/pages/rebound2.py
@@ -1,10 +1,14 @@
 import rebound
 import streamlit as st
 
+sim = rebound.Simulation()
+
 options = st.multiselect(
     'What are your favorite colors',
     ['Mercury', 'Venus', 'Earth', 'Mars'])
+
 sim.add("Sun")
+
 if 'Mercury' in options:
     sim.add("Mercury")
 if 'Venus' in options:
