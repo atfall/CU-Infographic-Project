f954452d2312990bea07163b6333e47c7f0490d7
atfall <61741874+atfall@users.noreply.github.com>
2022-11-15 17:09:56 +0000

    Update rebound3.py

--- webapp/pages/rebound3.py
+++ webapp/pages/rebound3.py
@@ -1,42 +1,30 @@
 import rebound
-import matplotlib.pyplot as plt
-import numpy as np
+import streamlit as st
+import datetime
+import pandas as pd
 
 sim = rebound.Simulation()
-sim.add("Sun") #Star A
-sim.add("Jupiter")
-sim.add("Earth")
-sim.add("Saturn")
-sim.move_to_com()
-fig = rebound.OrbitPlot(sim)
 
+options = st.selectbox(
+    'Add Planets',
+    'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter')
 
-fig, ax = plt.subplots(figsize=(5,5))
-ax.set_aspect("equal")
-ps = sim.particles
+d = st.date_input(
+    "Pick a date",
+    value = pd.to_datetime('today'))
+d = str(d)
+sim.add("Sun")
 
-# manually set plot boundaries
-lim = 2.3 
-ax.set_xlim([-lim, lim])
-ax.set_ylim([-lim, lim])
-
-# plot the stars and planets with separate symbols
-for star in ps[:2]:
-    ax.scatter(star.x, star.y, s=35, marker='*', facecolor='black', zorder=3)
-    
-for planet in ps[2:]:
-    ax.scatter(planet.x, planet.y, s=10, facecolor='black', zorder=3)
-    
-# Now individually plot orbit trails with appropriate orbit
-
-from rebound.plotting import fading_line
-
-ABb = ps[2] # circumbinary planet, use default jacobi coordinates
-o = np.array(ABb.sample_orbit())
-lc = fading_line(o[:,0], o[:,1])
-ax.add_collection(lc)
-
-Bb = ps[3] # planet in orbit around B, assign it as primary
-o = np.array(Bb.sample_orbit(primary=ps[1]))
-lc = fading_line(o[:,0], o[:,1])
-ax.add_collection(lc);
+if 'Mercury' in options:
+    sim.add("Mercury", date = d)
+if 'Venus' in options:
+    sim.add("Venus", date = d)
+if 'Earth' in options:
+    sim.add("Earth", date = d)
+if 'Jupiter' in options:
+    sim.add("Jupiter", date = d)
+ 
+fig, ax = rebound.OrbitPlot(sim)
+st.pyplot(fig)
+st.write('You selected:', options)
+st.write('You selected:', d)
