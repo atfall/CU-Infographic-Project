492566bc7d50ccde48b95ff2d5b795b736aacf53
atfall <61741874+atfall@users.noreply.github.com>
2022-11-15 16:06:21 +0000

    Update rebound2.py

--- webapp/pages/rebound2.py
+++ webapp/pages/rebound2.py
@@ -7,15 +7,10 @@ options = st.multiselect(
     ['Jupiter', 'Saturn', 'Uranus', 'Neptune'])
 
 def planets(choices):
-    match choices:
-        case 'Mercury':
-            sim.add("Mercury")
-        case 'Venus':
-            sim.add("Venus")
-        case 'Earth':
-            sim.add("Earth")
-        case _:
+    if 'Mercury' in choices:
+        sim.add("Mercury")
+ 
 
-    
+def(options) 
 fig = rebound.OrbitPlot(sim)
 st.pyplot(fig)
