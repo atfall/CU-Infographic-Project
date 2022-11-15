45dab2eaed24bf97f262886221c6a2dbf0780189
atfall <61741874+atfall@users.noreply.github.com>
2022-11-15 16:20:06 +0000

    Update rebound2.py

--- webapp/pages/rebound2.py
+++ webapp/pages/rebound2.py
@@ -5,12 +5,12 @@ options = st.multiselect(
     'What are your favorite colors',
     ['Mercury', 'Venus', 'Earth', 'Mars'])
 
-#def planets(choices):
-#    if 'Mercury' in choices:
-#        sim.add("Mercury")
+def planets(choices):
+    if 'Mercury' in choices:
+        sim.add("Mercury")
  
-#if options is not null:
-#    planets(options) 
-#fig = rebound.OrbitPlot(sim)
-#st.pyplot(fig)
+if options is not null:
+    planets(options) 
+fig = rebound.OrbitPlot(sim)
+st.pyplot(fig)
 st.write('You selected:', options)
