84ad983146e417b01fb6582f1835d8bd7bed62fd
atfall <61741874+atfall@users.noreply.github.com>
2022-11-28 13:21:26 +0000

    Create rebound5.py

---
+++ webapp/pages/rebound5.py
@@ -0,0 +1,51 @@
+import rebound
+import streamlit as st
+import datetime
+import pandas as pd
+import mpld3
+import streamlit.components.v1 as components
+import plotly.express as pe
+#%matplotlib notebook
+
+sim = rebound.Simulation()
+
+mercury = st.checkbox('Mercury')
+venus = st.checkbox('Venus')
+earth = st.checkbox('Earth', value = True)
+mars = st.checkbox('Mars')
+jupiter = st.checkbox('Jupiter')
+saturn = st.checkbox('Saturn')
+uranus = st.checkbox('Uranus')
+neptune = st.checkbox('Neptune')
+
+d = st.date_input(
+    "Pick a date",
+    value = pd.to_datetime('today'))
+d = str(d)
+sim.add("Sun")
+
+if mercury:
+    sim.add("Mercury", date = d)
+if venus:
+    sim.add("Venus", date = d)
+if earth:
+    sim.add("Earth", date = d)
+if mars:
+    sim.add("Mars", date = d)
+if jupiter:
+    sim.add("Jupiter", date = d)
+if saturn:
+    sim.add("Saturn", date = d)
+if uranus:
+    sim.add("Uranus", date = d)
+if neptune:
+    sim.add("Neptune", date = d)
+ 
+fig, ax = rebound.OrbitPlot(sim)
+st.pyplot(fig)
+fig = op.fig
+for i in range(100):
+    op.sim.integrate(sim.t+0.6)
+    op.update()       # update data
+    fig.canvas.draw() # redraw figure
+st.plotly_chart(fig)
