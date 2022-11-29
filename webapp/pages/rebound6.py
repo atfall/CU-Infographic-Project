dd1d18fbf29f312770f5ea3db55843ae2ce9a1be
atfall <61741874+atfall@users.noreply.github.com>
2022-11-29 13:41:50 +0000

    Update rebound6.py

--- webapp/pages/rebound6.py
+++ webapp/pages/rebound6.py
@@ -9,14 +9,21 @@ import matplotlib.pyplot as plt
 sim_in = rebound.Simulation()
 sim_out = rebound.Simulation()
 
-mercury = st.checkbox('Mercury')
-venus = st.checkbox('Venus')
-earth = st.checkbox('Earth', value = True)
-mars = st.checkbox('Mars')
-jupiter = st.checkbox('Jupiter')
-saturn = st.checkbox('Saturn')
-uranus = st.checkbox('Uranus')
-neptune = st.checkbox('Neptune')
+col1, col2= st.columns(2)
+
+with col_in:
+   st.header("Inner Solar System")
+   mercury = st.checkbox('Mercury')
+    venus = st.checkbox('Venus')
+    earth = st.checkbox('Earth', value = True)
+    mars = st.checkbox('Mars')
+
+with col_out:
+   st.header("Outer Solar System")
+    jupiter = st.checkbox('Jupiter')
+    saturn = st.checkbox('Saturn')
+    uranus = st.checkbox('Uranus')
+    neptune = st.checkbox('Neptune')
 
 d = st.date_input(
     "Pick a date",
@@ -34,6 +41,7 @@ if earth:
     sim_in.add("Earth", date = d)
 if mars:
     sim_in.add("Mars", date = d)
+    
 if jupiter:
     sim_out.add("Jupiter", date = d)
 if saturn:
