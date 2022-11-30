6c4524221c35a85fa60d20ac85c6bed91d30c88c
atfall <61741874+atfall@users.noreply.github.com>
2022-11-30 14:29:02 +0000

    Update rebound_A2.py

--- webapp/pages/rebound_A2.py
+++ webapp/pages/rebound_A2.py
@@ -20,12 +20,13 @@ d = st.date_input(
     value = pd.to_datetime('today'))
 d = str(d)
 
-col1, col2, col3= st.columns(3)
+
 
 st.write("Planets")
 planets = st.checkbox('Planets', value = True)
 
 st.write("Dwarf Planets")
+col1, col2, col3= st.columns(3)
 with col1:
    ceres = st.checkbox('Ceres', value = True)
    orcus = st.checkbox('Orcus', value = True)
