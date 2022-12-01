35ae93868bce6bc7eccd5a6eb55a6ef95b5dd6df
atfall <61741874+atfall@users.noreply.github.com>
2022-12-01 18:02:22 +0000

    Update reboundB2.py

--- webapp/pages/reboundB2.py
+++ webapp/pages/reboundB2.py
@@ -161,7 +161,11 @@ with col_in:
 with col_out:
     st.header("Outer Solar System")
     st.pyplot(op2.fig)
-
+animate = st.checkbox('Animate')
 #step1 = st.button('step1', on_click = step1())	
 #step2 = st.button('step2', on_click = step2())	
 #step3 = st.button('step3', on_click = step3())	
+if animate:
+    step1()
+if animate:
+    st.experimental_rerun()
