ed6a59953621b76179db987176913fb7b14fe104
atfall <61741874+atfall@users.noreply.github.com>
2022-12-01 17:15:03 +0000

    Update reboundB2.py

--- webapp/pages/reboundB2.py
+++ webapp/pages/reboundB2.py
@@ -154,6 +154,6 @@ with col_out:
     st.pyplot(op2.fig)
 
 step1 = st.button('step1', on_click = step1())	
-step2 = st.button('step2', on_click = step2())	
-step3 = st.button('step3', on_click = step3())	
+#step2 = st.button('step2', on_click = step2())	
+#step3 = st.button('step3', on_click = step3())	
 
