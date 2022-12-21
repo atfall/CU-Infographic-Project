e233d6b7d35aa856c38aa543b84bf02d34974c64
atfall <61741874+atfall@users.noreply.github.com>
2022-12-21 12:33:00 +0000

    Update 🪐_rebound_C1.py

--- webapp/🪐_rebound_C1.py
+++ webapp/🪐_rebound_C1.py
@@ -208,7 +208,7 @@ if step_btn_3:
 	step3()
 
 st.write(f'Time Elapsed: {st.session_state.count} Days')
-current_date = date + pd.DateOffset(days=st.session_state.count).date
+current_date = date + pd.DateOffset(days=st.session_state.count)
 st.write(f'Current Date: {current_date}')
 
 #plotting
